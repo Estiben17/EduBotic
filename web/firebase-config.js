d9c711468f974e2d0810b8e807a03e9cf9293b2b
Estiben17 <estibendavila1705@gmail.com>
2025-10-13 12:12:03 -0500

    cambio

---
+++ web/firebase-config.js
@@ -0,0 +1,9 @@
+﻿const firebaseConfig = {
+  apiKey: "AIzaSyC_5OzuPzVM1unKaUzZN3obe4MWW6TgHaI",
+  authDomain: "edubotic-4a675.firebaseapp.com",
+  projectId: "edubotic-4a675", 
+  storageBucket: "edubotic-4a675.firebasestorage.app",
+  messagingSenderId: "850963688325",
+  appId: "1:850963688325:web:be94723cb3d226ec0f72ec",
+  measurementId: "G-PGRRZ6B89M"
+};
